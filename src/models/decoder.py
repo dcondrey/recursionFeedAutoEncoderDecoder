fe5b75e61d2a2760035fc491fce51b971ef8803b
David Condrey <davidcondrey@me.com>
2023-11-02 11:30:05 -0700

    variable layer structure

--- src/models/decoder.py
+++ src/models/decoder.py
@@ -1,13 +1,15 @@
 # src/models/decoder.py
 
 import tensorflow as tf
-from tensorflow.keras.layers import Input, Dense, BatchNormalization, Dropout
+from tensorflow.keras.layers import Input, Dense, BatchNormalization, Dropout, LeakyReLU
 
-def build_decoder(latent_dim, output_dim):
+def build_decoder(latent_dim, output_dim, layers=[64, 128], dropout_rate=0.5):
     input_layer = Input(shape=(latent_dim,))
-    x = Dense(64, activation='relu')(input_layer)
-    x = BatchNormalization()(x)
-    x = Dropout(0.5)(x)
-    x = Dense(128, activation='relu')(x)
+    x = input_layer
+    for units in layers:
+        x = Dense(units)(x)
+        x = LeakyReLU()(x)
+        x = BatchNormalization()(x)
+        x = Dropout(dropout_rate)(x)
     decoded = Dense(output_dim, activation='sigmoid')(x)
     return tf.keras.Model(inputs=input_layer, outputs=decoded)
--- src/models/encoder.py
+++ src/models/encoder.py
@@ -1,13 +1,15 @@
 # src/models/encoder.py
 
 import tensorflow as tf
-from tensorflow.keras.layers import Input, Dense, BatchNormalization, Dropout
+from tensorflow.keras.layers import Input, Dense, BatchNormalization, Dropout, LeakyReLU
 
-def build_encoder(input_dim, latent_dim):
+def build_encoder(input_dim, latent_dim, layers=[128, 64], dropout_rate=0.5):
     input_layer = Input(shape=(input_dim,))
-    x = Dense(128, activation='relu')(input_layer)
-    x = BatchNormalization()(x)
-    x = Dropout(0.5)(x)
-    x = Dense(64, activation='relu')(x)
+    x = input_layer
+    for units in layers:
+        x = Dense(units)(x)
+        x = LeakyReLU()(x)
+        x = BatchNormalization()(x)
+        x = Dropout(dropout_rate)(x)
     encoded = Dense(latent_dim, activation='relu')(x)
     return tf.keras.Model(inputs=input_layer, outputs=encoded)
